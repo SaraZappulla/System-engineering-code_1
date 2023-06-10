860240857b9cb12406023e07c929b767d8636751
Sara Lucia Zappulla <saralucia.zappulla@mail.polimi.it>
2023-06-10 12:58:06 +0200

    second part of OBDH

--- OBDH.m
+++ OBDH.m
@@ -45,3 +45,32 @@ code_ADCS = (code_RW*4+code_thruster*8+code_ST*2+code_CSS*10+code_MIMU+code_kalm
 RAM=(data_ADCS+data_telemetry+data_TCS+data_EPS+code_ADCS+code_EPS+code_telemetry+code_TCS) ... 
     *PC_bitwordsratio/8/1000
 %smaller thna PC_RAM: design ideal
+
+%outputs: OBDH memory + OBC frequency and throughtputs by similarity. 
+facquisition=1; %MIL bus: 1MHz
+facquisitionSW=50; %SpaceWire: 400Mbit/s=50MB/s=50MHz
+
+KIPS_RW=facquisition/2*60
+KIPS_MIMU=facquisition/10*75 %rate gyros
+KIPS_ST=facquisition/0.01*45
+KIPS_thrusters=facquisition/2*10
+KIPS_CSS=facquisition/1*10
+KIPS_Kalman=facquisition/2*800
+KIPS_error=facquisition/10*120
+KIPS_attdet=facquisition/10*900
+KIPS_attcntrl=facquisition/10*900
+KIPS_orbitprop=facquisition/1*200
+KIPS_eph=facquisition/0.5*20
+
+
+KIPS_EPS=facquisition/1*45
+
+KIPS_telemetry=facquisitionSW/10*320
+
+KIPS_TCS=facquisitionSW/0.1*25
+margin=5; %400%
+
+KIPS_ADCS=KIPS_eph+KIPS_orbitprop+KIPS_attcntrl+KIPS_attdet+KIPS_error+...
+    KIPS_Kalman+KIPS_CSS+KIPS_thrusters+KIPS_ST+KIPS_MIMU+KIPS_RW
+KIPS_TOT=KIPS_ADCS+KIPS_TCS+KIPS_EPS+KIPS_telemetry
+KIPS_TOT_margined=KIPS_TOT*margin/1000 %measured in MIPS
